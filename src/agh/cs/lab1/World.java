662c9437a654c612de70c77542ee1793f1813171
speedy <k.w1@o2.pl>
2020-02-12 16:36:57 +0100

    Nearly done lab1

--- src/agh/cs/lab1/World.java
+++ src/agh/cs/lab1/World.java
@@ -1,34 +1,40 @@
 package agh.cs.lab1;
 
-
-
 public class World {
     public static void main(String[] args) {
         System.out.println("Start");
-        Direction[] directions = new Direction[7];
-        String[] direction_s = {"f", "r", "r", "l", "b", "f", "r"};
-        for (int i=0; i<direction_s.length; i++){
-            switch (direction_s[i]){
+
+        Direction[] directions = new Direction[args.length];
+        int j = 0;
+        for (String arg: args){
+            switch (arg){
                 case "f" :
-                    directions[i] = Direction.f;
+                    directions[j] = Direction.f;
+                    j++;
                     break;
                 case "b" :
-                    directions[i] = Direction.b;
+                    directions[j] = Direction.b;
+                    j++;
+                    break;
                 case "r" :
-                    directions[i] = Direction.r;
+                    directions[j] = Direction.r;
+                    j++;
                     break;
                 case "l" :
-                    directions[i] = Direction.l;
+                    directions[j] = Direction.l;
+                    j++;
+                    break;
+                default:
                     break;
             }
         }
         run(directions);
-        System.out.println("\nKoniec");
+        System.out.println("\nStop");
     }
     public static void run (Direction[] directions){
-        System.out.println("Zwierzak idzie do przodu");
-        for(int i=0; i<directions.length; i++){
-            switch (directions[i]){
+        int j = 0;
+        for(Direction direction: directions){
+            switch (directions[j]){
                 case f :
                     System.out.println("Zwierzak idzie do przodu");
                     break;
@@ -39,9 +45,12 @@ public class World {
                     System.out.println("Zwierzak skręca w prawo");
                     break;
                 case l :
-                    System.out.println("Zwierzak skręca w lewo,");
+                    System.out.println("Zwierzak skręca w lewo");
+                    break;
+                default:
                     break;
             }
+            j++;
         }
     }
 }
