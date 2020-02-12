fd374affa15228a113b9b581bc811b0fe9320fc7
speedy <k.w1@o2.pl>
2020-02-12 19:03:28 +0100

    Lab1 done

--- src/agh/cs/lab1/Direction.java
+++ src/agh/cs/lab1/Direction.java
@@ -1,8 +1,15 @@
 package agh.cs.lab1;
 
 public enum Direction {
-    f,
-    b,
-    r,
-    l
+    F, B, R, L;
+
+    public static Direction changeDirection (String direction){
+        switch (direction){
+            case "f" : return Direction.F;
+            case "b" : return Direction.B;
+            case "l" : return Direction.L;
+            case "r" : return Direction.R;
+            default: return null;
+        }
+    }
 }
--- src/agh/cs/lab1/World.java
+++ src/agh/cs/lab1/World.java
@@ -1,56 +1,36 @@
 package agh.cs.lab1;
+import java.util.stream.Stream;
+
 
 public class World {
     public static void main(String[] args) {
         System.out.println("Start");
 
-        Direction[] directions = new Direction[args.length];
-        int j = 0;
-        for (String arg: args){
-            switch (arg){
-                case "f" :
-                    directions[j] = Direction.f;
-                    j++;
-                    break;
-                case "b" :
-                    directions[j] = Direction.b;
-                    j++;
-                    break;
-                case "r" :
-                    directions[j] = Direction.r;
-                    j++;
-                    break;
-                case "l" :
-                    directions[j] = Direction.l;
-                    j++;
-                    break;
-                default:
-                    break;
-            }
-        }
+        Direction[] directions = Stream.of(args)
+                .map(Direction::changeDirection)
+                .filter(direction -> direction != null)
+                .toArray(Direction[]::new);
+
         run(directions);
-        System.out.println("\nStop");
+
+        System.out.println("Stop");
     }
     public static void run (Direction[] directions){
-        int j = 0;
         for(Direction direction: directions){
-            switch (directions[j]){
-                case f :
+            switch (direction){
+                case F :
                     System.out.println("Zwierzak idzie do przodu");
                     break;
-                case b :
+                case B :
                     System.out.println("Zwierzak idzie do tyłu");
                     break;
-                case r :
+                case R :
                     System.out.println("Zwierzak skręca w prawo");
                     break;
-                case l :
+                case L :
                     System.out.println("Zwierzak skręca w lewo");
                     break;
-                default:
-                    break;
             }
-            j++;
         }
     }
 }
